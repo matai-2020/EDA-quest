d10b44ea9c59b27f1659ee2c70b199d143f44428
Keenen Leyson <keenen.leyson@gmail.com>
2020-07-16 16:20:03 +1200

    change logo size on scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,8 @@ export default class App extends React.Component {
   render () {
     return (
       <div style={{ textAlign: 'center' }}>
-        <h1>Hello World</h1>
+        {/* <h1>Hello World</h1> */}
+        <img id="logo" src="src/assets/quest-logo.png" alt="Quest Logo" />
         <HighScore />
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -19,6 +19,19 @@ let config = {
   scene: playGame
 }
 
+window.onscroll = function () { scrollFunction() }
+
+function scrollFunction () {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById('logo').style.width = '200px'
+    document.getElementById('logo').style.marginBottom = '10px'
+
+  } else {
+    document.getElementById('logo').style.width = '800px'
+    document.getElementById('logo').style.marginBottom = '200px'
+  }
+}
+
 const game = new Phaser.Game(config)
 
 ReactDOM.render(
