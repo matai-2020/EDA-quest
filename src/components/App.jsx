c5e2cf3ee8756e95a8ba0bfb5960bb91088de369
Keenen Leyson <keenen.leyson@gmail.com>
2020-07-19 10:08:13 +1200

    added new game logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <div style={{ textAlign: 'center' }}>
         {/* <h1>Hello World</h1> */}
-        <img id="logo" src="/assets/Game/quest-logo.png" alt="Quest Logo" />
+        <img id="logo" src="/assets/Game/eda-quest-logo.png" alt="Quest Logo" />
       </div>
     )
   }
