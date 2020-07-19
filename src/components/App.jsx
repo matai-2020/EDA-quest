0e261fd13eafc199c5221e39ccf16dc6180e0322
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-07-19 17:02:20 +1200

    add victory component to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HighScore from './HighScore.jsx'
+import Victory from './Victory.jsx'
 
 export default class App extends React.Component {
   render () {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
         {/* <h1>Hello World</h1> */}
         {/* <img id="logo" src="/assets/Game/eda-quest-logo.png" alt="Quest Logo" /> */}
         <HighScore />
+        <Victory />
       </div>
     )
   }
