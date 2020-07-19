84706f31c41420e3d372f0831e607425834b2465
Louis <louis.fowler@gmail.com>
2020-07-20 10:05:30 +1200

    fixed logo scroll function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import HighScore from './HighScore.jsx'
 
 window.onscroll = function () {
+  console.log(window.scrollY)
+  console.log('test')
   scrollFunction()
 }
 
 function scrollFunction () {
-  if (document.body.scrollTop >= 1 || document.documentElement.scrollTop >= 1) {
-    document.getElementById('logo').style.width = '10px'
-    document.getElementById('logo').style.visibility = 'hidden'
-  } else {
+  if (window.scrollY === 0) {
     document.getElementById('logo').style.visibility = 'visible'
     document.getElementById('logo').style.width = '900px'
     document.getElementById('logo').style.marginTop = '150px'
     document.getElementById('logo').style.marginBottom = '230px'
+  } else {
+    document.getElementById('logo').style.width = '10px'
+    document.getElementById('logo').style.visibility = 'hidden'
   }
 }
 
 export default class App extends React.Component {
   render () {
+    console.log(window.scrollY)
     return (
       <div style={{ textAlign: 'center' }}>
         {/* <h1>Hello World</h1> */}
