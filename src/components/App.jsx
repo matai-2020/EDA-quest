b861eef861ac9f05709a175ad826d626e3f1e3e5
Louis <louis.fowler@gmail.com>
2020-07-20 08:34:16 +1200

    update sprites

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import HighScore from './HighScore.jsx'
 
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction () {
+  if (document.body.scrollTop >= 1 || document.documentElement.scrollTop >= 1) {
+    document.getElementById('logo').style.width = '10px'
+    document.getElementById('logo').style.visibility = 'hidden'
+  } else {
+    document.getElementById('logo').style.visibility = 'visible'
+    document.getElementById('logo').style.width = '900px'
+    document.getElementById('logo').style.marginTop = '150px'
+    document.getElementById('logo').style.marginBottom = '230px'
+  }
+}
+
 export default class App extends React.Component {
   render () {
     return (
