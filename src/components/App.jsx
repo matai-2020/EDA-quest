1933128062bf0e027e1b0630b9160328453ff503
Louis <louis.fowler@gmail.com>
2020-07-20 10:21:15 +1200

    fixed scroll function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,12 @@ window.onscroll = function () {
 }
 
 function scrollFunction () {
-  if (window.scrollY === 0) {
-    document.getElementById('logo').style.visibility = 'visible'
-    document.getElementById('logo').style.width = '900px'
-    document.getElementById('logo').style.marginTop = '150px'
-    document.getElementById('logo').style.marginBottom = '230px'
-  } else {
+  if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
     document.getElementById('logo').style.width = '10px'
     document.getElementById('logo').style.visibility = 'hidden'
+  } else {
+    document.getElementById('logo').style.visibility = 'visible'
+    document.getElementById('logo').style.width = '900px'
   }
 }
 
@@ -25,7 +23,9 @@ export default class App extends React.Component {
     return (
       <div style={{ textAlign: 'center' }}>
         {/* <h1>Hello World</h1> */}
-        <img id="logo" src="/assets/Game/eda-quest-logo.png" alt="Quest Logo" />
+        <div className="logoContainer">
+          <img id="logo" src="/assets/Game/eda-quest-logo.png" alt="Quest Logo" />
+        </div>
         <HighScore />
       </div>
     )
