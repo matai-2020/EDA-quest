56d6d9e7a06a168abfe408a5ac061235665f0816
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-07-22 13:52:14 +1200

    start react testing

---
+++ src/components/HighScore.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import '@testing-library/extend-expect'
+
+import HighScore from './HighScore'
+
+test('HighScore component only renders if game is over',)
